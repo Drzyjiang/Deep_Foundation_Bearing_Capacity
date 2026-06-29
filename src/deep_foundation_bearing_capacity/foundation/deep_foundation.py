bcbd0a3be72060a2e827176c37dc19013a562549
Drzyjiang <zyjiang@alumni.iastate.edu>
2026-06-28 19:15:02 -0500

    add: add sanity check on arg segments for deep_foundation class

--- src/deep_foundation_bearing_capacity/foundation/deep_foundation.py
+++ src/deep_foundation_bearing_capacity/foundation/deep_foundation.py
@@ -1,1 +1,31 @@
-#
+# Classes for deep foundations
+
+from deep_foundation_bearing_capacity.segments.segments import Segment
+
+
+class deep_foundation:
+    def __init__(self, segments: list[Segment]):
+        '''
+        Args:
+            segments (Segment): list a of segments, in order of from top to bottom 
+        '''
+
+
+        
+
+        self.segments = segments
+
+    def _sanity_check_on_segments(self, segments: list[Segment])->bool:
+        '''
+        Sanity check on segments
+        '''
+
+        if not isinstance(segments, list):
+            raise TypeError("ERROR: segments shall be a list")
+        
+        for segment in segments:
+            if not isinstance(segment, Segment):
+                raise TypeError("ERROR: all segments shall be type Segment.")
+            
+        return True
+
