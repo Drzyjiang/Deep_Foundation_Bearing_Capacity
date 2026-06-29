39467014fd98d9b6d2152ab25da05b89f5dfada7
Drzyjiang <zyjiang@alumni.iastate.edu>
2026-06-28 19:54:15 -0500

    add: add sanity check for top_depth in DeepFoundation class

--- src/deep_foundation_bearing_capacity/foundation/deep_foundation.py
+++ src/deep_foundation_bearing_capacity/foundation/deep_foundation.py
@@ -1,10 +1,11 @@
 # Classes for deep foundations
 
+from deep_foundation_bearing_capacity.constants.constants import SCALAR_TYPE
 from deep_foundation_bearing_capacity.segments.segments import Segment
 
 
-class deep_foundation:
-    def __init__(self, segments: list[Segment], top_depth: float = 0, resistance_correction:bool = True):
+class DeepFoundation:
+    def __init__(self, segments: list[Segment], top_depth: SCALAR_TYPE = 0, resistance_correction:bool = True):
         '''
         Args:
             segments (Segment): list a of segments, in order of from top to bottom 
@@ -14,9 +15,11 @@ class deep_foundation:
 
         self.segments = segments
 
+    
 
 
-    def _sanity_check_on_segments(self, segments: list[Segment])->bool:
+
+    def _sanity_check_segments(self, segments: list[Segment])->bool:
         '''
         Sanity check on segments
         '''
@@ -28,6 +31,16 @@ class deep_foundation:
             if not isinstance(segment, Segment):
                 raise TypeError("ERROR: all segments shall be type Segment.")
             
+        return True
+    
+    def _sanity_check_top_depth(self, top_depth: SCALAR_TYPE)->bool:
+        '''
+        Sanity check on top depth
+        
+        '''
+        if not isinstance(top_depth, SCALAR_TYPE):
+            raise TypeError(f"ERROR: top_depth shall be type {SCALAR_TYPE}")
+        
         return True
     
     def _side_resistance_correction(self):
@@ -42,7 +55,7 @@ class deep_foundation:
 
         # Between the ground surface and 5 ft, alpha is set to zero
         # to account for seasonal moisture change
-        
+
 
     def _accumulative_side_resistance(self):
         '''
