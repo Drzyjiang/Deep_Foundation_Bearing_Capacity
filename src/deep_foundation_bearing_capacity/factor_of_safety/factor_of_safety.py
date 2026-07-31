ce6ab5f883aa6b46ddb94937fda6592525573030
Drzyjiang <zyjiang@alumni.iastate.edu>
2026-07-31 15:16:54 -0500

    add: add tests for factor_of_safety.py

--- src/deep_foundation_bearing_capacity/factor_of_safety/factor_of_safety.py
+++ src/deep_foundation_bearing_capacity/factor_of_safety/factor_of_safety.py
@@ -1,6 +1,6 @@
 # class for factor of safety
 
-from deep_foundation_bearing_capacity.constants import constants
+from deep_foundation_bearing_capacity.constants.constants import NUMERIC_TYPE
 
 
 class FactorOfSafety:
@@ -8,10 +8,10 @@ class FactorOfSafety:
     Class for factor of safety of general foundations, including shallow and deep foundations
 
     '''
-    def __init__(self, factor_of_safety:constants.SCALAR_TYPE = 3.0):
+    def __init__(self, factor_of_safety: NUMERIC_TYPE = 3.0):
         """
         Args:
-            factor_of_safety (constants.SCALAR_TYPE): for shallow foundation, typically use 3.0
+            factor_of_safety (NUMERIC_TYPE): for shallow foundation, typically use 3.0
         """
         
         # sanity check
@@ -30,10 +30,10 @@ class FactorOfSafety:
             result (bool): True or False
         '''
 
-        if not isinstance(fs, constants.SCALAR_TYPES):
-            raise TypeError("ERROR: fs shall be type {constants.SCALAR_TYPES}.")
-        elif fs <= 0:
-            raise ValueError("ERROR: fs shall be greater than zero.")
+        if not isinstance(fs, NUMERIC_TYPE):
+            raise TypeError("ERROR: fs shall be type {NUMERIC_TYPE}.")
+        elif fs < 1.0:
+            raise ValueError("ERROR: fs shall be not less than unity.")
         else:
             return True
 
@@ -42,7 +42,7 @@ class FactorOfSafetyDeepFoundation (FactorOfSafety):
     Class for deep foundations
     
     '''
-    def __init__(self, factor_of_safety:constants.SCALAR_TYPE = 3.0, fs_end_bearing:constants.SCALAR_TYPE = 3.0):
+    def __init__(self, factor_of_safety: NUMERIC_TYPE = 3.0, fs_end_bearing: NUMERIC_TYPE = 3.0):
         '''
         Args:
             fs (constants.SCALAR_TYPE): used for skin friction
---
+++ tests/test_factor_of_safety.py
@@ -0,0 +1,58 @@
+# Test factor_of_safety.factor_of_safety.py
+import numpy as np
+import pytest
+
+from deep_foundation_bearing_capacity.factor_of_safety.factor_of_safety import (
+    FactorOfSafety,
+    FactorOfSafetyDeepFoundation,
+)
+
+# =====================
+# 1. Test FactorOfSafety
+# =====================
+
+class TestFactorOfSafety:
+    def test_construction(self):
+        """
+        Test construction of class FactorOfSafety
+        """
+        fs = FactorOfSafety(3.0)
+        assert fs.factor_of_safety == 3.0
+
+    def test_sanity_check(self):
+        """
+        Test sanity_check
+        """
+        with pytest.raises(ValueError, match = "fs"):
+            FactorOfSafety(0.9)
+
+
+# =====================
+# 2. Test FactorOfSafetyDeepFoundation
+# ===================== 
+class TestFactorOfSafetyDeepFoundation:
+    def test_construction(self):
+        """
+        Test construction of class FactorOfSafetyDeepFoundation
+        """
+        fs = FactorOfSafetyDeepFoundation(factor_of_safety = 3.0, fs_end_bearing=2.5 )
+        assert fs.fs_deep_foundation_skin_compression == 3.0
+        assert fs.fs_deep_foundation_skin_uplift == 3.0
+        assert fs.fs_deep_foundation_end == 2.5
+
+    @pytest.mark.parametrize("fs_side", [np.array([2.0])])
+    @pytest.mark.parametrize("fs_end_bearing", [np.array([2.5])])
+    def test_numpy(self, fs_side, fs_end_bearing):
+        """
+        Test input format of numpy
+        """
+        fs = FactorOfSafetyDeepFoundation(fs_side, fs_end_bearing)
+        assert fs.fs_deep_foundation_skin_compression == fs_side
+        assert fs.fs_deep_foundation_end == fs_end_bearing
+
+    def test_inheritance(self):
+        """
+        Test FactorOfSafetyDeepFoundation is a child of FactorOfSafety
+        """
+        fs = FactorOfSafetyDeepFoundation()
+        assert isinstance(fs, FactorOfSafety)
