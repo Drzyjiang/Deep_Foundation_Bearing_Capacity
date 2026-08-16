ec783aa48db785c7c930169dfc26d1d52f349ff2
Drzyjiang <zyjiang@alumni.iastate.edu>
2026-08-16 08:45:28 -0400

    fix: move data/ to notebooks/

--- tests/conftest.py
+++ tests/conftest.py
@@ -186,7 +186,7 @@ def layer_typical_1(stiff_clay):
     """
     A typical layer with clay
     """
-    return Layer(layer_index = 0, geomaterial = stiff_clay, ground_water_depth  = 0,
+    return Layer(layer_index = 0, geomaterial = stiff_clay, ground_water_depth = 0,
                  top_depth = 0, thickness = 10)
 
 @pytest.fixture
@@ -194,7 +194,7 @@ def layer_typical_2(loose_sand):
     """
     A typical layer with sand
     """
-    return Layer(layer_index = 1, geomaterial = loose_sand, ground_water_depth  = 0,
+    return Layer(layer_index = 1, geomaterial = loose_sand, ground_water_depth = 0,
                  top_depth = 10, thickness = 10)
 
 @pytest.fixture
@@ -202,7 +202,7 @@ def layer_typical_3(competent_rock):
     """
     A typical layer with compent rock
     """
-    return Layer(layer_index = 2, geomaterial = competent_rock, ground_water_depth  = 0,
+    return Layer(layer_index = 2, geomaterial = competent_rock, ground_water_depth = 0,
                  top_depth = 20, thickness = 10)
 
 @pytest.fixture
